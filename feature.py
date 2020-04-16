5448cdae7230434d32d4defb662cfbd4002d6da2
Hugo Fonseca <hugo.fonseca@grupoorion.eng.br>
2020-04-16 04:22:52 -0300

    Skipping cmd and table args solving

--- feature.py
+++ feature.py
@@ -15,11 +15,12 @@ class Feature:
     def process_file(self, steps, env_variables, locale, features_dict):
         current_feature = ""
         current_scenario = ""
+        current_skipped = ""
 
         for line in self.file_content:
             statement_match = re.match(RegularExpressions.regexps['statement'], line, re.IGNORECASE | re.MULTILINE)
             verb_match = re.match(RegularExpressions.regexps['verb'], line, re.IGNORECASE | re.MULTILINE)
-            table_match = re.match(RegularExpressions.regexps['verb'], line, re.IGNORECASE | re.MULTILINE)
+            table_match = re.match(RegularExpressions.regexps['table'], line, re.IGNORECASE | re.MULTILINE)
 
             if statement_match:
                 groups = statement_match.groups()
@@ -32,7 +33,7 @@ class Feature:
                         if check_indentation((indentation, env_variables['tab_size'], 0)):
                             current_feature = self.process_feature(name, features_dict)
                         else:
-                            self.logger.error("Unexpected indent at:\n\t%s\n\u2191\u2191\u2191\u2191" % line)
+                            self.logger.error("Unexpected indent at statement:\n\t%s\n\u2191\u2191\u2191\u2191" % line)
                             return ErrorCodes.SYNTAX_ERROR
 
                     elif localized_statement == 'scenario':
@@ -40,12 +41,15 @@ class Feature:
                             if check_indentation((indentation, env_variables['tab_size'], 1)):
                                 current_scenario = self.process_scenario(name, features_dict, current_feature)
                             else:
-                                self.logger.error("Unexpected indent at:\n\t%s\n\u2191\u2191\u2191\u2191" % line)
+                                self.logger.error("Unexpected indent at statement:\n\t%s\n\u2191\u2191\u2191\u2191"
+                                                  % line)
                                 return ErrorCodes.SYNTAX_ERROR
                         else:
                             self.logger.error("Scenario statement (%s) without a previous Feature" % line)
+
                     elif localized_statement == 'factory':
                         self.logger.warning('Factories are not implemented yet')
+
                     else:
                         self.logger.critical(
                             'Unknown translation "%s" for statement %s' % (localized_statement, statement))
@@ -60,40 +64,57 @@ class Feature:
                 indentation = len(groups[0])
                 verb = groups[1].lower()
                 step_name = groups[2]
-
+                print(verb, step_name)
                 if Feature.is_verb(locale, verb):
+                    verb = locale['verbs'][verb]
                     if check_indentation((indentation, env_variables['tab_size'], 2)):
                         scenario = features_dict[current_feature]['scenarios'][current_scenario]
-                        print(scenario)
-                        step_attr = Feature.process_step_name(step_name)
 
-                        try:
-                            step_ref = self.get_step_ref(current_feature, steps, locale['verbs'][verb],
-                                                         step_attr['method_name'])
-                            step_attr['verb'] = verb
-                            step_attr['ref'] = step_ref
-                            step_attr['status'] = ExecutionStatus.PENDING_EXECUTION
-                        except (AttributeError, KeyError):
-                            self.logger.warning("Undefined step (below feature %s): %s" % (current_feature, step_name))
-                            step_attr['status'] = ExecutionStatus.MISSING_STEP
+                        if verb == 'do':
+                            cmd = step_name.lower()
+                            if cmd == 'skip':
+                                print("%s scenario skipped", current_scenario)
+                                current_skipped = current_scenario
+                                scenario['status'] = ExecutionStatus.SKIPPED
+                        if current_skipped != current_scenario:
+                            step_attr = Feature.process_step_name(step_name)
 
-                        finally:
-                            print(step_attr)
-                            scenario['steps'].append(step_attr)
+                            try:
+                                step_ref = self.get_step_ref(current_feature, steps, locale['verbs'][verb],
+                                                             step_attr['method_name'])
+                                step_attr['verb'] = verb
+                                step_attr['ref'] = step_ref
+                                step_attr['status'] = ExecutionStatus.PENDING_EXECUTION
+                            except (AttributeError, KeyError):
+                                self.logger.warning("Undefined step (below feature %s): %s" % (current_feature, step_name))
+                                step_attr['status'] = ExecutionStatus.MISSING_STEP
+
+                            finally:
+                                scenario['steps'].append(step_attr)
 
                     else:
-                        self.logger.error("Unexpected indent at:\n\t%s\n\u2191\u2191\u2191\u2191" % line)
+                        self.logger.error("Unexpected indent at verb:\n\t%s\n\u2191\u2191\u2191\u2191" % line)
                         return ErrorCodes.SYNTAX_ERROR
 
                 else:
                     if check_indentation((indentation, env_variables['tab_size'], 1)):
                         self.update_feature_desc(verb, step_name, features_dict, current_feature)
                     else:
-                        self.logger.error("Unexpected indent at:\n\t%s\n\u2191\u2191\u2191\u2191" % line)
+                        self.logger.error("Unexpected indent at description:\n\t%s\n\u2191\u2191\u2191\u2191" % line)
                         return ErrorCodes.SYNTAX_ERROR
             elif table_match:
-                print("Table")
-
+                if current_skipped != current_scenario:
+                    groups = table_match.groups()
+                    indentation = len(groups[0])
+                    key = groups[1]
+                    value = groups[2]
+                    if check_indentation((indentation, env_variables['tab_size'], 3)):
+                        steps_arr = features_dict[current_feature]['scenarios'][current_scenario]['steps']
+                        last_args_arr = steps_arr[len(steps_arr)-1]['args']
+                        if len(last_args_arr):
+                            last_args_arr[len(last_args_arr)-1][key] = value
+                        else:
+                            last_args_arr.append({key: value})
             else:
                 self.logger.error("Syntax error at expression %s" % line)
 
